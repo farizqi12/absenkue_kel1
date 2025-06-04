4e0d5afb6bd58cb64f5d6f5d61fa837841857142
farizqi <muhamadfarizqi39@gmail.com>
2025-06-04 08:09:24 +0700

    Add Profil controller and views for user profiles

---
+++ app/Http/Controllers/Profil.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Profil extends Controller
+{
+    public function showFarizqi()
+    {
+        return view('profil.farizqi');
+    }
+     public function showYudha()
+    {
+        return view('profil.');
+    }
+     public function showSoviki()
+    {
+        return view('profil.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profil/farizqi', [App\Http\Controllers\ProfilFarizqi::class, 'show'])
+    ->name('profil.farizqi');
